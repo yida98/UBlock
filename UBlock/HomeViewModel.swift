3efd8dfaa567b85fa403dc0c2a71709582a177e1
Yida Da <carriedada@gmail.com>
2021-04-01 16:47:37 -0700

    matches protocol

--- UBlock/HomeViewModel.swift
+++ UBlock/HomeViewModel.swift
@@ -15,38 +15,25 @@ class HomeViewModel: ObservableObject {
     var apps: AlphabetizedList<AppFile>
     let query = NSMetadataQuery()
     
+    private let fileManager = FileManager.default
+    
     init() {
         self.apps = AlphabetizedList<AppFile>()
         findApps()
     }
-    
-    private var cancellableSet: Set<AnyCancellable> = []
-    
+        
     private func findApps() {
-        let fileManager = FileManager.default
         
         let applicationsURLs = fileManager.urls(for: .allApplicationsDirectory, in: .allDomainsMask)
         let filteredURLs = applicationsURLs.filter { fileManager.fileExists(atPath: $0.path) }
         for filteredURL in filteredURLs {
             do {
-                let results = try fileManager.contentsOfDirectory(at: filteredURL, includingPropertiesForKeys: [URLResourceKey.thumbnailKey], options: FileManager.DirectoryEnumerationOptions.skipsHiddenFiles)
+                let results = try fileManager.contentsOfDirectory(at: filteredURL, includingPropertiesForKeys: nil, options: [])
                 let filteredResults = results.filter { $0.path.hasSuffix(".app") }
 
-                for url in filteredResults {
-                    generateThumbnail(for: url)
-                        .sink(receiveCompletion: { (completion) in
-                            switch completion {
-                                case .failure:
-                                    debugPrint("[ERROR] Thumbnail generation error!!")
-                                case .finished:
-                                    debugPrint("Finished generating thumbnail...")
-                            }
-
-                        }, receiveValue: {
-                            self.apps.insert(AppFile(name: fileManager.displayName(atPath: url.path), image: $0))
-                            self.apps.reindex()
-                        })
-                        .store(in: &cancellableSet)
+                for appURL in filteredResults {
+                    
+                    apps.insert(AppFile(name: fileManager.displayName(atPath: appURL.path), image: getAppIcon(url: appURL)))
                 }
             } catch {
                 print("Error at \(filteredURL)! \(error)")
@@ -55,54 +42,80 @@ class HomeViewModel: ObservableObject {
         
     }
     
-    private func generateThumbnail(for url: URL) -> AnyPublisher<NSImage, Error> {
-        return Future<NSImage, Error> { promise in
-            
-            let size: CGSize = CGSize(width: 10, height: 90)
-            
-            // Create the thumbnail request.
-            let request = QLThumbnailGenerator.Request(fileAt: url,
-                                                       size: size,
-                                                       scale: 1,
-                                                       representationTypes: .all)
-            
-            let generator = QLThumbnailGenerator.shared
-            generator.generateBestRepresentation(for: request) { (thumbnailRepresentation, error) in
-                if let thumbnail = thumbnailRepresentation {
-                    promise(.success(thumbnail.nsImage))
-                } else if let er = error {
-                    promise(.failure(er))
-                }
+    private func getAppIcon(url: URL) -> NSImage {
+        guard let infoPlistURL = findInfoPlist(appURL: url) else {
+            fatalError("[ERROR] \(url.path) has no info.plist!!")
+        }
+        
+        if let iconName = getPlistObject(infoPlistURL, key: "CFBundleIconFile") as? String {
+            let iconRule = FileRule(value: iconName, rule: .endsWith)
+            if let iconURL = fileManager.recursiveGetFirstOccurence(url, matchRules: [iconRule]) {
+                return iconURL.asNSImage() ?? NSImage()
             }
-
-        }.eraseToAnyPublisher()
+        }
+        
+        return NSImage()
     }
-//
-//    private func fetchApps() {
-//        let predicate = NSPredicate(format: "kMDItemContentType == 'com.apple.application-bundle'")
-//
-//        NotificationCenter.default.addObserver(self, selector: #selector(queryDidFinish(_:)), name: NSNotification.Name.NSMetadataQueryDidFinishGathering, object: nil)
-//        query.predicate = predicate
-//        query.start()
-//    }
-//
-//    @objc
-//    private func queryDidFinish(_ notification: NSNotification) {
-//        print("Did receive")
-//        for result in query.results {
-//            guard let item = result as? NSMetadataItem else {
-//                print("NOT METADATAITEM?!")
-//                continue
-//            }
-//            print("result: \(item.value(forAttribute: kMDItemDisplayName as String) ?? "")")
-//        }
-//    }
-//
+    
+    private func findInfoPlist(appURL: URL) -> URL? {
+        
+        let rule = FileRule(value: "Info.plist", rule: .endsWith)
+        
+        let result = fileManager.recursiveGetFirstOccurence(appURL, matchRules: [rule])
+        
+        return result
+    }
+    
+    private func getPlistObject(_ plistURL: URL, key: Any) -> Any? {
+        guard let dictionary = NSDictionary(contentsOf: plistURL) else {
+            fatalError("[ERROR] NOT AN INFO.PLIST!!")
+        }
+        
+        return dictionary.object(forKey: key)
+    }
 }
 
-extension URL {
-    var isDirectory: Bool {
-        let values = try? resourceValues(forKeys: [.isDirectoryKey])
-        return values?.isDirectory ?? false
+extension FileManager {
+    
+    func getContents(_ url: URL, with components: String = "",propertiesForKeys: [URLResourceKey]?, options: DirectoryEnumerationOptions) -> [URL] {
+        var fullURL = url
+        fullURL.appendPathComponent(components)
+        do {
+            let contents = try self.contentsOfDirectory(at: fullURL, includingPropertiesForKeys: propertiesForKeys, options: options)
+            return contents
+        } catch {
+            debugPrint("Error")
+        }
+        return [URL]()
     }
+    
+    func recursiveGetFirstOccurence(_ url: URL, matchRules: [FileRule]) -> URL? {
+        let contents = getContents(url, propertiesForKeys: [], options: [])
+        
+        for content in contents {
+            if url.matches(matchRules: matchRules) { return url }
+            if content.isDirectory {
+                return recursiveGetFirstOccurence(url, matchRules: matchRules)
+            }
+        }
+        
+        return nil
+    }
+    
+    func recursiveGetContents(_ url: URL, matchRules: [FileRule]) -> [URL] {
+        var result = [URL]()
+        let contents = getContents(url, propertiesForKeys: [], options: [])
+        
+        for content in contents {
+            if content.isDirectory {
+                result.append(contentsOf: recursiveGetContents(content, matchRules: matchRules))
+            }
+            if url.matches(matchRules: matchRules) {
+                result.append(content)
+            }
+        }
+        
+        return result
+    }
+    
 }
---
+++ UBlock/URL+ext.swift
@@ -0,0 +1,74 @@
+//
+//  URL+ext.swift
+//  UBlock
+//
+//  Created by Yida Zhang on 2021-04-01.
+//
+
+import Foundation
+import AppKit
+
+extension URL {
+    var isDirectory: Bool {
+        print(self.path)
+        let values = try? resourceValues(forKeys: [.isDirectoryKey])
+        return values?.isDirectory ?? false
+    }
+}
+
+protocol MatchRule {
+    associatedtype RuleType
+    
+    func matches(matchRule: RuleType) -> Bool
+}
+    
+struct FileRule {
+    var value: String
+    var rule: FileRule.Rule
+    
+    enum Rule {
+        case endsWith
+        case startsWith
+        case contains
+    }
+}
+
+extension URL: MatchRule {
+    typealias RuleType = FileRule
+    
+    func matches(matchRule: FileRule) -> Bool {
+        switch matchRule.rule {
+        case FileRule.Rule.endsWith:
+            return path.hasSuffix(matchRule.value)
+        case FileRule.Rule.startsWith:
+            return path.hasPrefix(matchRule.value)
+        case FileRule.Rule.contains:
+            return path.contains(matchRule.value)
+        }
+    }
+    
+    func matches(matchRules: [FileRule]) -> Bool {
+        for matchRule in matchRules {
+            if !matches(matchRule: matchRule) {
+                return false
+            }
+        }
+        return true
+    }
+
+}
+
+extension URL {
+    func asNSImage() -> NSImage? {
+        do {
+            let data = try Data(contentsOf: self)
+            if let icon = NSImage(data: data) {
+                icon.size = CGSize(width: 60, height: 90)
+                return icon
+            }
+        } catch {
+            print(error)
+        }
+        return nil
+    }
+}
