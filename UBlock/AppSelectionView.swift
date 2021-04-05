11a84bbb4ceff2414f4d3d3b0169b7741caa3398
Yida Da <carriedada@gmail.com>
2021-04-05 14:41:37 -0700

    new views

---
+++ UBlock/AppSelectionView.swift
@@ -0,0 +1,20 @@
+//
+//  AppSelectionView.swift
+//  UBlock
+//
+//  Created by Yida Zhang on 2021-04-05.
+//
+
+import SwiftUI
+
+struct AppSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppSelectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppSelectionView()
+    }
+}
---
+++ UBlock/AppView.swift
@@ -0,0 +1,20 @@
+//
+//  AppView.swift
+//  UBlock
+//
+//  Created by Yida Zhang on 2021-04-05.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppView()
+    }
+}
