d60294ac6cf765c7974ad117cab2832dcb42ef03
Yida Da <carriedada@gmail.com>
2021-05-04 19:33:10 -0700

    some commits

--- UBlock/Views/ContentView.swift
+++ UBlock/Views/ContentView.swift
@@ -19,6 +19,14 @@ struct ContentView: View {
                     LogView()
                     Spacer()
                 }
+            }.toolbar {
+                ToolbarItem(placement: ToolbarItemPlacement.navigation) {
+                    Button(action: {
+                        
+                    }) {
+                        Image(systemName: "sidebar.left")
+                    }
+                }
             }
             RulesView().environmentObject(RulesViewModel(rules: [Rule(category: AppCategory(name: "Entertainment"), weekday: [.weekday], start: Time(hour: 0, min: 0, sec: 0), end: Time(hour: 3, min: 0, sec: 0))]))
         }
